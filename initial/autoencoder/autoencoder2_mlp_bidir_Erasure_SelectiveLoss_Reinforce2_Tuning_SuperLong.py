ea86015d95a615db5cb76676d6f9ec26bbd7f496
Michael Hahn <mhahn29@gmail.com>
2020-06-29 17:04:48 +0800

    code

--- initial/autoencoder/autoencoder2_mlp_bidir_Erasure_SelectiveLoss_Reinforce2_Tuning_SuperLong.py
+++ initial/autoencoder/autoencoder2_mlp_bidir_Erasure_SelectiveLoss_Reinforce2_Tuning_SuperLong.py
@@ -6,14 +6,25 @@ print("Character aware!")
 # Character-aware version of the `Tabula Rasa' language model
 # char-lm-ud-stationary-vocab-wiki-nospaces-bptt-2-words_NoNewWeightDrop.py
 # Adopted for English and German
+
 import sys
+import random
+import torch
+print(torch.__version__)
+
+import math
+from torch.autograd import Variable
+import time
+import corpusIteratorWikiWords
+import sys
+import time
+
 
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--language", dest="language", type=str, default="english")
 parser.add_argument("--load-from-autoencoder", dest="load_from_autoencoder", type=str, default=878921872)
 
-import random
 
 parser.add_argument("--batchSize", type=int, default=random.choice([128])) # , 128, 128, 256
 parser.add_argument("--word_embedding_size", type=int, default=random.choice([512]))
@@ -45,20 +56,20 @@ parser.add_argument("--entropy_weight", type=float, default=0.0) #random.choice(
 
 model = "REAL_REAL"
 
-import math
 
 args=parser.parse_args()
 
 
+
+
 print(args.myID)
-import sys
+# Redirect output to a file (I did this for experiments via SLURM)
 sys.stdout = open("/u/scr/mhahn/reinforce-logs/full-logs/"+__file__+"_"+str(args.myID), "w")
 
 print(args)
 
 
 
-import corpusIteratorWikiWords
 
 
 
@@ -68,9 +79,13 @@ def plus(it1, it2):
    for x in it2:
       yield x
 
-char_vocab_path = "vocabularies/"+args.language.lower()+"-wiki-word-vocab-50000.txt"
 
-with open(char_vocab_path, "r") as inFile:
+#############################################################
+# Vocabulary
+
+vocab_path = "vocabularies/"+args.language.lower()+"-wiki-word-vocab-50000.txt"
+
+with open(vocab_path, "r") as inFile:
      itos = [x.split("\t")[0] for x in inFile.read().strip().split("\n")[:50000]]
 stoi = dict([(itos[i],i) for i in range(len(itos))])
 
@@ -79,75 +94,41 @@ itos_total = ["<SOS>", "<EOS>", "OOV"] + itos
 stoi_total = dict([(itos_total[i],i) for i in range(len(itos_total))])
 
 
-with open("vocabularies/char-vocab-wiki-"+args.language, "r") as inFile:
-     itos_chars = [x for x in inFile.read().strip().split("\n")]
-stoi_chars = dict([(itos_chars[i],i) for i in range(len(itos_chars))])
 
-
-itos_chars_total = ["<SOS>", "<EOS>", "OOV"] + itos_chars
-
-
-import random
-
-
-import torch
-
-print(torch.__version__)
-
-#from weight_drop import WeightDrop
-
+############################################################
+# Model
 
 rnn_encoder = torch.nn.LSTM(2*args.word_embedding_size, int(args.hidden_dim/2.0), args.layer_num, bidirectional=True).cuda()
 rnn_decoder = torch.nn.LSTM(2*args.word_embedding_size, args.hidden_dim, args.layer_num).cuda()
-
-
-
-
 output = torch.nn.Linear(args.hidden_dim, len(itos)+3).cuda()
-
 word_embeddings = torch.nn.Embedding(num_embeddings=len(itos)+3, embedding_dim=2*args.word_embedding_size).cuda()
 
 logsoftmax = torch.nn.LogSoftmax(dim=2)
-
 attention_softmax = torch.nn.Softmax(dim=1)
-
 
 train_loss = torch.nn.NLLLoss(ignore_index=0)
 print_loss = torch.nn.NLLLoss(size_average=False, reduce=False, ignore_index=0)
 char_dropout = torch.nn.Dropout2d(p=args.char_dropout_prob)
 
-
-#train_loss_chars = torch.nn.NLLLoss(ignore_index=0, reduction='sum')
-
-
 attention_proj = torch.nn.Linear(args.hidden_dim, args.hidden_dim, bias=False).cuda()
-#attention_layer = torch.nn.Bilinear(args.hidden_dim, args.hidden_dim, 1, bias=False).cuda()
 attention_proj.weight.data.fill_(0)
-
 
 output_mlp = torch.nn.Linear(2*args.hidden_dim, args.hidden_dim).cuda()
 
 modules_autoencoder = [rnn_decoder, rnn_encoder, output, word_embeddings, attention_proj, output_mlp]
 
-
+#################################################################
+# Noise model
 memory_mlp_inner = torch.nn.Linear(2*args.word_embedding_size, 500).cuda()
 memory_mlp_outer = torch.nn.Linear(500, 1).cuda()
 
 sigmoid = torch.nn.Sigmoid()
 relu = torch.nn.ReLU()
 
-#character_embeddings = torch.nn.Embedding(num_embeddings = len(itos_chars_total)+3, embedding_dim=args.char_emb_dim).cuda()
-#
-#char_composition = torch.nn.LSTM(args.char_emb_dim, args.char_enc_hidden_dim, 1, bidirectional=True).cuda()
-#char_composition_output = torch.nn.Linear(2*args.char_enc_hidden_dim, args.word_embedding_size).cuda()
-#
-#char_decoder_rnn = torch.nn.LSTM(args.char_emb_dim + args.hidden_dim, args.char_dec_hidden_dim, 1).cuda()
-#char_decoder_output = torch.nn.Linear(args.char_dec_hidden_dim, len(itos_chars_total))
-#
-#
-#modules_autoencoder += [character_embeddings, char_composition, char_composition_output, char_decoder_rnn, char_decoder_output]
 
 modules_memory = [memory_mlp_inner, memory_mlp_outer]
+
+###################################################################
 
 def parameters_memory():
    for module in modules_memory:
@@ -167,12 +148,7 @@ learning_rate = args.learning_rate
 
 optim = torch.optim.SGD(parameters_memory(), lr=learning_rate, momentum=args.momentum) # 0.02, 0.9
 
-#named_modules_autoencoder = {"rnn" : rnn, "output" : output, "word_embeddings" : word_embeddings, "optim" : optim}
 
-#if args.load_from is not None:
-#  checkpoint = torch.load(MODELS_HOME+"/"+args.load_from+".pth.tar")
-#  for name, module in named_modules_autoencoder.items():
- #     module.load_state_dict(checkpoint[name])
 if args.load_from_autoencoder is not None:
   try:
      checkpoint = torch.load("/u/scr/mhahn/CODEBOOKS/"+args.language+"_"+"autoencoder2_mlp_bidir_Erasure_SelectiveLoss.py"+"_code_"+str(args.load_from_autoencoder)+".txt")
@@ -181,53 +157,28 @@ if args.load_from_autoencoder is not None:
   for i in range(len(checkpoint["components"])):
       modules_autoencoder[i].load_state_dict(checkpoint["components"][i])
 
-from torch.autograd import Variable
 
-
-# ([0] + [stoi[training_data[x]]+1 for x in range(b, b+sequence_length) if x < len(training_data)]) 
-
-#from embed_regularize import embedded_dropout
-
-relu = torch.nn.ReLU()
+##########################################################################
+# Encode dataset chunks into tensors
 
 def prepareDatasetChunks(data, train=True):
       numeric = [0]
       count = 0
       print("Prepare chunks")
       numerified = []
-      numerified_chars = []
       for chunk in data:
-       #print(len(chunk))
        for char in chunk:
-#         if char == " ":
- #          continue
          count += 1
-#         if count % 100000 == 0:
-#             print(count/len(data))
          numerified.append((stoi[char]+3 if char in stoi else 2))
-         numerified_chars.append([0] + [stoi_chars[x]+3 if x in stoi_chars else 2 for x in char])
-
        if len(numerified) > (args.batchSize*args.sequence_length):
          sequenceLengthHere = args.sequence_length
-
          cutoff = int(len(numerified)/(args.batchSize*sequenceLengthHere)) * (args.batchSize*sequenceLengthHere)
          numerifiedCurrent = numerified[:cutoff]
-         numerifiedCurrent_chars = numerified_chars[:cutoff]
-
-         for i in range(len(numerifiedCurrent_chars)):
-            numerifiedCurrent_chars[i] = numerifiedCurrent_chars[i][:15] + [1]
-            numerifiedCurrent_chars[i] = numerifiedCurrent_chars[i] + ([0]*(16-len(numerifiedCurrent_chars[i])))
-
-
          numerified = numerified[cutoff:]
-         numerified_chars = numerified_chars[cutoff:]
-       
          numerifiedCurrent = torch.LongTensor(numerifiedCurrent).view(args.batchSize, -1, sequenceLengthHere).transpose(0,1).transpose(1,2).cuda()
-         numerifiedCurrent_chars = torch.LongTensor(numerifiedCurrent_chars).view(args.batchSize, -1, sequenceLengthHere, 16).transpose(0,1).transpose(1,2).cuda()
-
          numberOfSequences = numerifiedCurrent.size()[0]
          for i in range(numberOfSequences):
-             yield numerifiedCurrent[i], numerifiedCurrent_chars[i]
+             yield numerifiedCurrent[i], None
          hidden = None
        else:
          print("Skipping")
@@ -237,121 +188,99 @@ def prepareDatasetChunks(data, train=True):
 
 
 hidden = None
-
 zeroBeginning = torch.LongTensor([0 for _ in range(args.batchSize)]).cuda().view(1,args.batchSize)
 beginning = None
-
-zeroBeginning_chars = torch.zeros(1, args.batchSize, 16).long().cuda()
-
-
 zeroHidden = torch.zeros((args.layer_num, args.batchSize, args.hidden_dim)).cuda()
 
+# Dropout masks
 bernoulli = torch.distributions.bernoulli.Bernoulli(torch.tensor([0.1 for _ in range(args.batchSize)]).cuda())
-
 bernoulli_input = torch.distributions.bernoulli.Bernoulli(torch.tensor([1-args.weight_dropout_in for _ in range(args.batchSize * 2 * args.word_embedding_size)]).cuda())
 bernoulli_output = torch.distributions.bernoulli.Bernoulli(torch.tensor([1-args.weight_dropout_out for _ in range(args.batchSize * 2 * args.hidden_dim)]).cuda())
 
 
-#runningAveragePredictionLoss = 1.0
 runningAverageReward = 1.0
-
-
 expectedRetentionRate = 0.5
 
 def forward(numeric, train=True, printHere=False):
       global beginning
-      global beginning_chars
       if True:
           beginning = zeroBeginning
-          beginning_chars = zeroBeginning_chars
 
-      numeric, numeric_chars = numeric
+      numeric, _ = numeric
 
 
+      # the true input
       numeric = torch.cat([beginning, numeric], dim=0)
-
+      # word embeddings for the true input (to be fed into noise model)
       embedded_everything = word_embeddings(numeric)
 
-
+      # computing the noise model
       memory_hidden = sigmoid(memory_mlp_outer(relu(memory_mlp_inner(embedded_everything))))
-     # print(memory_hidden)
-    #  print(memory_hidden.size())
-#      bernoulli_memory = torch.distributions.bernoulli.Bernoulli(memory_hidden)
-
-   #   memory_filter = bernoulli_memory.sample()
+      # Sampling a mask from the noise model
       memory_filter = torch.bernoulli(input=memory_hidden)
-
+      # logprobabilities of the actions of the noise model
       bernoulli_logprob = torch.where(memory_filter == 1, torch.log(memory_hidden+1e-10), torch.log(1-memory_hidden+1e-10))
-
       bernoulli_logprob_perBatch = bernoulli_logprob.mean(dim=0)
+
+      # If necessary, calculate entropy of decisions (for regularization purposes)
       if args.entropy_weight > 0:
          entropy = -(memory_hidden * torch.log(memory_hidden+1e-10) + (1-memory_hidden) * torch.log(1-memory_hidden+1e-10)).mean()
       else:
          entropy=-1.0
- #     print(bernoulli_logprob)
-  #    print(bernoulli_logprob.size())
-   #   print(bernoulli_logprob_perBatch)
 
-#      quit()
-#memory_mlp_inner = torch.nn.Linear(2*args.word_embedding_size, 500)
-#memory_mlp_outer = torch.nn.Linear(500, 1)
-#
-#sigmoid = torch.nn.Sigmoid()
-#relu = torch.nn.ReLU()
 
       memory_filter = memory_filter.squeeze(2)
- #     print(memory_filter.size(), numeric.size())
-#      quit()
+      # Run the noise model
       numeric_noised = torch.where(memory_filter==1, numeric, 0*numeric) #[[x if random.random() > args.deletion_rate else 0 for x in y] for y in numeric.cpu().t()]
-#      numeric_noised = torch.LongTensor([[0 for _ in range(args.sequence_length-len(y))] + y for y in numeric_noised]).cuda().t()
+      # true input in those places where it noised, and 0 elsewhere
       numeric_onlyNoisedOnes = torch.where(memory_filter == 0, numeric, 0*numeric) # target is 0 in those places where no noise has happened
 
+      # Input to the decoding RNN
       input_tensor = Variable(numeric[:-1], requires_grad=False)
+      # Input for the encoding RNN
       input_tensor_noised = Variable(numeric_noised[:-1], requires_grad=False)
+      # Target for the decoding RNN
 
       target_tensor = Variable(numeric_onlyNoisedOnes[1:], requires_grad=False)
 
-
+      # Encode input for the decoding RNN
       embedded = word_embeddings(input_tensor)
-      if False and train:
-         embedded = char_dropout(embedded)
-         mask = bernoulli_input.sample()
-         mask = mask.view(1, args.batchSize, 2*args.word_embedding_size)
-         embedded = embedded * mask
-
+#      if False and train:
+#         embedded = char_dropout(embedded)
+#         mask = bernoulli_input.sample()
+#         mask = mask.view(1, args.batchSize, 2*args.word_embedding_size)
+#         embedded = embedded * mask
+      # Encode input for the encoding RNN
       embedded_noised = word_embeddings(input_tensor_noised)
-      if False and train:
-         embedded_noised = char_dropout(embedded_noised)
-         mask = bernoulli_input.sample()
-         mask = mask.view(1, args.batchSize, 2*args.word_embedding_size)
-         embedded_noised = embedded_noised * mask
-#      print(input_tensor_noised)
- #     print("NOISED", embedded_noised[1,1,])
-  #    print("NOISED", embedded_noised[1,2,])
-   #   print("NOISED", embedded_noised[2,2,])
+#      if False and train:
+#         embedded_noised = char_dropout(embedded_noised)
+#         mask = bernoulli_input.sample()
+#         mask = mask.view(1, args.batchSize, 2*args.word_embedding_size)
+#         embedded_noised = embedded_noised * mask
 
-    #  print(embedded_noised.size())
-      
+      # Run both encoder and decoder
       out_encoder, _ = rnn_encoder(embedded_noised, None)
       out_decoder, _ = rnn_decoder(embedded, None)
 
+      # Have the decoder attend to the encoder
       attention = torch.bmm(attention_proj(out_encoder).transpose(0,1), out_decoder.transpose(0,1).transpose(1,2))
       attention = attention_softmax(attention).transpose(0,1)
       from_encoder = (out_encoder.unsqueeze(2) * attention.unsqueeze(3)).sum(dim=0).transpose(0,1)
       out_full = torch.cat([out_decoder, from_encoder], dim=2)
 
-
-      if False and train:
-        mask = bernoulli_output.sample()
-        mask = mask.view(1, args.batchSize, 2*args.hidden_dim)
-        out_full = out_full * mask
-
+#      if False and train:
+#        mask = bernoulli_output.sample()
+#        mask = mask.view(1, args.batchSize, 2*args.hidden_dim)
+#        out_full = out_full * mask
 
 
+
+      # Obtain logits for reconstruction
       logits = output(relu(output_mlp(out_full) ))
+      # Obtain log-probabilities
       log_probs = logsoftmax(logits)
 
-      # Prediction Loss 
+      # Calculate loss. Here, due to the ignore_index=0 argument in the definition of train_loss, loss is incurred only for recovering those words that had been noised.
       lossTensor = print_loss(log_probs.view(-1, len(itos)+3), target_tensor.view(-1)).view(-1, args.batchSize)
       negativeRewardsTerm1 = lossTensor.mean(dim=0)
 
@@ -361,15 +290,15 @@ def forward(numeric, train=True, printHere=False):
       # Overall Reward
       negativeRewardsTerm = negativeRewardsTerm1 + args.RATE_WEIGHT * negativeRewardsTerm2
 
-#      global runningAveragePredictionLoss
       global runningAverageReward
+      global expectedRetentionRate
 
       loss = ((negativeRewardsTerm.detach()-runningAverageReward) * bernoulli_logprob_perBatch).mean()
       if args.entropy_weight > 0:
          loss -= args.entropy_weight  * entropy
-      global expectedRetentionRate
       expectedRetentionRate = memory_hidden.mean()
 
+      # Occasionally print
       if printHere:
          losses = lossTensor.data.cpu().numpy()
          numericCPU = numeric.cpu().data.numpy()
@@ -396,7 +325,6 @@ def backward(loss, printHere):
 
 lossHasBeenBad = 0
 
-import time
 
 totalStartTime = time.time()
 
