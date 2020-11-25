75f2527d6bdc3465c7c53152678b91dc95d206f4
Michael Hahn <mhahn29@gmail.com>
2020-11-25 11:48:45 +0800

    code

---
+++ initial/lm/trainAttention/parsed_output/evaluate.R
@@ -0,0 +1,18 @@
+data = read.csv("collectCompletionParseStats.py.txt", sep="\t")
+
+counts = read.csv("../../../../../forgetting/fromCorpus_counts.csv", sep="\t")
+counts$Ratio = counts$True_False - counts$False_False
+
+
+
+data = merge(data, counts, by=c("Noun"), all.x=TRUE)
+
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+plot = ggplot(data %>% group_by(DeletionRate, Noun, Ratio) %>% summarise(Subj=mean(Subj/(Subj+NotSubj))), aes(x=Ratio, y=Subj)) + geom_point() + facet_wrap(~DeletionRate)
+
+plot = ggplot(data %>% group_by(DeletionRate, Noun, Ratio) %>% summarise(That=mean(That/(That+NoThat))), aes(x=Ratio, y=That)) + geom_point() + facet_wrap(~DeletionRate)
+
+
