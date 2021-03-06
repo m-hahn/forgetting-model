3ca7987230895008ce8143191a766c1457fe138e
Michael Hahn <mhahn29@gmail.com>
2021-03-05 16:19:33 -0800

    code

--- initial/lm/trainAttention/TUNE_char-lm-ud-stationary_12_SuperLong_WithAutoencoder_VeryLong_WithSurp_Agr1.py
+++ initial/lm/trainAttention/TUNE_char-lm-ud-stationary_12_SuperLong_WithAutoencoder_VeryLong_WithSurp_Agr1.py
@@ -19,14 +19,15 @@ scripts = []
 #scripts.append("char-lm-ud-stationary_12_SuperLong_WithAutoencoder_WithEx_Samples_Short_Combination_Subseq_VeryLong_WithSurp12_Sampling_GPT2.py")
 
 #scripts.append("char-lm-ud-stationary_12_SuperLong_WithAutoencoder_WithEx_Samples_Short_Combination_Subseq_VeryLong_WithSurp12_Long.py") # good (long but that's okay)
-scripts.append("char-lm-ud-stationary_12_SuperLong_WithAutoencoder_WithEx_Samples_Short_Combination_Subseq_VeryLong_WithSurp12_NormJudg_Agr1.py")
+#scripts.append("char-lm-ud-stationary_12_SuperLong_WithAutoencoder_WithEx_Samples_Short_Combination_Subseq_VeryLong_WithSurp12_NormJudg_Agr1.py")
+scripts.append("char-lm-ud-stationary_12_SuperLong_WithAutoencoder_WithEx_Samples_Short_Combination_Subseq_VeryLong_WithSurp12_NormJudg_Short.py")
 
 #scripts.append("char-lm-ud-stationary_12_SuperLong_WithAutoencoder_WithEx_Samples_Short_Combination_Subseq_VeryLong_WithSurp13.py")
 
 for i in range(100): # 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 
 # 0.35, 0.4, 0.45, 0.5, 0.55, 
     # 0.15, 0.2, 0.25, 0.3, 0.35 , 
-   deletion_rate = str(random.choice([0.35, 0.4, 0.4, 0.4, 0.45])) #, 0.5, 0.6, 0.7]))     #[0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6]))
+   deletion_rate = str(random.choice([0.0, 0.1, 0.2, 0.3, 0.4, 0.5])) #, 0.5, 0.6, 0.7]))     #[0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6]))
    predictability_weight = str(random.choice([0.0, 0.0, 0.0, 0.5])) #, 0.5, 0.6, 0.7]))     #[0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6]))
    command = ["/u/nlp/anaconda/main/anaconda3/envs/py37-mhahn/bin/python", random.choice(scripts), "--tuning=1", "--deletion_rate="+deletion_rate, "--predictability_weight="+predictability_weight]
    print(command)
