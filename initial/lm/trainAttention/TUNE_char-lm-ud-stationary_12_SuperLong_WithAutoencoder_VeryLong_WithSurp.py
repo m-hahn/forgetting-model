ca7e41009ded30600453b087539a75e3c55eccc6
Michael Hahn <mhahn29@gmail.com>
2021-04-04 16:55:57 -0700

    code

--- initial/lm/trainAttention/TUNE_char-lm-ud-stationary_12_SuperLong_WithAutoencoder_VeryLong_WithSurp.py
+++ initial/lm/trainAttention/TUNE_char-lm-ud-stationary_12_SuperLong_WithAutoencoder_VeryLong_WithSurp.py
@@ -39,6 +39,8 @@ scripts = []
 #scripts.append("char-lm-ud-stationary_12_SuperLong_WithAutoencoder_WithEx_Samples_Short_Combination_Subseq_VeryLong_WithSurp12_NormJudg_Short_Cond_Shift_NoComma_Bugfix_VNStims_3_W_GPT2.py")
 scripts.append("char-lm-ud-stationary_12_SuperLong_WithAutoencoder_WithEx_Samples_Short_Combination_Subseq_VeryLong_WithSurp12_NormJudg_Short_Cond_Shift_NoComma_Bugfix_VNStims_3_W_GPT2M.py")
 scripts.append("char-lm-ud-stationary_12_SuperLong_WithAutoencoder_WithEx_Samples_Short_Combination_Subseq_VeryLong_WithSurp12_NormJudg_Short_Cond_Shift_NoComma_Bugfix_VNStims_3_W_GPT2L.py")
+scripts.append("char-lm-ud-stationary_12_SuperLong_WithAutoencoder_WithEx_Samples_Short_Combination_Subseq_VeryLong_WithSurp12_NormJudg_Short_Cond_Shift_NoComma_Bugfix_W_GPT2M.py")
+scripts.append("char-lm-ud-stationary_12_SuperLong_WithAutoencoder_WithEx_Samples_Short_Combination_Subseq_VeryLong_WithSurp12_NormJudg_Short_Cond_Shift_NoComma_Bugfix_W_GPT2L.py")
 
 
 #scripts.append("char-lm-ud-stationary_12_SuperLong_WithAutoencoder_WithEx_Samples_Short_Combination_Subseq_VeryLong_WithSurp13.py")
@@ -53,6 +55,6 @@ for i in range(ITER): # 0.1, 0.15, 0.2, 0.25, 0.3, 0.35,
 # 0.35, 0.4, 0.45, 0.5, 0.55, 
     # 0.15, 0.2, 0.25, 0.3, 0.35 , 
    deletion_rate = str(random.choice([0.3, 0.4, 0.5]))  # , 0.6, 0.7    #[0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6]))
-   command = ["/u/nlp/anaconda/main/anaconda3/envs/py37-mhahn/bin/python", random.choice(scripts), "--tuning=1", "--deletion_rate="+deletion_rate, "--predictability_weight="+str(random.choice([0.0])), "--learning_rate_memory="+str(random.choice([0.00002, 0.00005, 0.0001])), "--learning_rate_autoencoder="+str(random.choice([0.001, 0.01, 0.1, 0.2])), "--momentum="+str(random.choice([0.5, 0.7, 0.9]))] #, "--predictability_weight=0.0"]
+   command = ["/u/nlp/anaconda/main/anaconda3/envs/py37-mhahn/bin/python", random.choice(scripts), "--tuning=1", "--deletion_rate="+deletion_rate, "--predictability_weight="+str(random.choice([0.0, 0.25, 0.5, 0.75])), "--learning_rate_memory="+str(random.choice([0.00002, 0.00005, 0.0001])), "--learning_rate_autoencoder="+str(random.choice([0.001, 0.01, 0.1, 0.2])), "--momentum="+str(random.choice([0.5, 0.7, 0.9]))] #, "--predictability_weight=0.0"]
    print(command)
    subprocess.call(command)
