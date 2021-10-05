2407f05fd96af3994cd54df1ab79c6eb52b81361
Michael Hahn <mhahn29@gmail.com>
2021-10-05 04:38:21 -0700

    code

--- initial/lm/trainAttention/collect12_NormJudg_Short_Cond_W_GPT2_ByTrial_E1.py
+++ initial/lm/trainAttention/collect12_NormJudg_Short_Cond_W_GPT2_ByTrial_E1.py
@@ -1,15 +1,17 @@
+import glob
 import codecs
 import os
 
 header = "Noun Item Region Condition Surprisal SurprisalReweighted ThatFraction ThatFractionReweighted".split(" ")
 header += ["S1", "S2", "Word", "Script", "ID", "predictability_weight", "deletion_rate", "autoencoder", "lm"]
 
+PATH0 = "/juice/scr/mhahn/reinforce-logs-both-short/results/"
 PATH = "/juice/scr/mhahn/reinforce-logs-both-short/full-logs/"
 PATH2 = "/juice/scr/mhahn/reinforce-logs-both-short/full-logs-tsv-perItem/"
 
 with open(f"{PATH2}/{__file__}.tsv", "w") as outFile:
  print("\t".join(header), file=outFile)
- for f in os.listdir(PATH):
+ for f in sorted(os.listdir(PATH2)):
    shib = "12_NormJudg_Short_Cond_Shift_NoComma_Bugfix"
    if shib in f:
       suffix = "script_"+f[f.index(shib)+len(shib):f.index(".py")]
@@ -17,10 +19,14 @@ with open(f"{PATH2}/{__file__}.tsv", "w") as outFile:
         continue
       print(f)
       print(suffix)
-#      accept = False
-      with codecs.open(PATH+f, "r", 'utf-8', "ignore") as inFile:
+      assert f.endswith("_Model")
+      modelID = f.split("_")[-2]
+      results_files = glob.glob(PATH0+"/*_"+modelID)
+      if len(results_files) == 0:
+         print("ERROR26: NO RESULTS FILE", f)
+         continue
+      with codecs.open(results_files[0], "r", 'utf-8', "ignore") as inFile:
          try:
-           iterations = next(inFile).strip()
            arguments = next(inFile).strip()
          except StopIteration:
            continue
@@ -41,15 +47,22 @@ with open(f"{PATH2}/{__file__}.tsv", "w") as outFile:
           predictability_weight = arguments["predictability_weight"]
           deletion_rate = arguments["deletion_rate"]
           try:
-           with open(PATH2+f+"_Model", "r") as inFile:
+           with open(PATH2+f, "r") as inFile:
              print("Opened", PATH2+f+"_Model")
              data = [x.split("\t") for x in inFile.read().strip().split("\n")]
              data = data[1:]
              for line in data:
                  if len(line) == 10:
                     print("WARNING: COLUMN MISSING!!!", line)
-                    _ = float(line[-1]) # Make sure the last entry is a number, as a basic sanity check
+                    try:
+                       _ = float(line[-1]) # Make sure the last entry is a number, as a basic sanity check
+                    except:
+                      print("ERROR 56", line)
+                      continue
                     line.append("NA")
+                 if len(line) != 11:
+                    print("ERROR", line)
+                    continue
                  assert len(line) == 11, line
                  print("\t".join(line + [suffix, arguments["myID"], arguments["predictability_weight"], arguments["deletion_rate"], arguments["load_from_autoencoder"], arguments["load_from_plain_lm"]]), file=outFile)
           except FileNotFoundError:
