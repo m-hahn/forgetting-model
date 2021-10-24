3db8de3fac5aa0a0ee320ebf7463acec388ba25a
Michael Hahn <mhahn29@gmail.com>
2021-10-23 22:30:41 -0700

    code

--- initial/lm/trainAttention/assembleHyperparameterResults.py
+++ initial/lm/trainAttention/assembleHyperparameterResults.py
@@ -18,17 +18,22 @@ for name in os.listdir(PATH):
       args = dict([x.split("=") for x in args.replace("Namespace(", "").rstrip(")").split(", ")])
       deletion_rate = args["deletion_rate"]
       predictability_weight = args["predictability_weight"]
+      args["script"] = name[:name.rfind("_")][-10:]
+#      print(name, args["script"])
+ #     quit()
       results[(predictability_weight, deletion_rate)].append((runningAverageReward, runningAveragePredictionLoss, runningAverageReconstructionLoss, args))
 
 
 with open(f"{__file__}.txt", "w") as outFile:
- for lambda_, delta_ in sorted(list(results)):
+ with open(f"{__file__}.tsv", "w") as outFileTSV:
+  for lambda_, delta_ in sorted(list(results)):
     scores = sorted(results[(lambda_, delta_)], key=lambda x:x[0], reverse=True)
     print("-------------------", file=outFile)
     print("-------------------")
     print(lambda_, delta_, file=outFile)
     print(lambda_, delta_)
     for x, a, b, y in scores:
+       print("\t".join([str(w) for w in [round(x/2, 2), round(a,2), round(b,2), y["learning_rate_memory"], y["learning_rate_autoencoder"], float(y.get("learning_rate_lm", 0)), y["momentum"], y["myID"], y["script"]]]), file=outFileTSV)
        print("\t".join([str(w) for w in [round(x/2, 2), round(a,2), round(b,2), y["learning_rate_memory"], y["learning_rate_autoencoder"], float(y.get("learning_rate_lm", 0)), y["momentum"], y["myID"]]]), file=outFile)
        print("\t".join([str(w) for w in [round(x/2, 2), round(a,2), round(b,2), y["learning_rate_memory"], y["learning_rate_autoencoder"], float(y.get("learning_rate_lm", 0)), y["momentum"], y["myID"]]]))
     
